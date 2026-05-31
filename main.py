ed91190703aae5835f37b02b673eb464378e8395
aysegulertem <aysglert0@gmail.com>
2026-05-31 15:04:57 +0300

    Refactor main.py to include data display and updates
    Display fire risk data and sensor readings in the Streamlit app.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import folium
-from streamlit_folium import st_folium
 from streamlit_autorefresh import st_autorefresh
 import warnings
 import winsound
